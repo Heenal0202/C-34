b5a7a6378c2abf722d01b4a9473b2c451b119567
Heenal0202 <74818075+Heenal0202@users.noreply.github.com>
2021-04-08 13:52:51 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,19 @@
+class Ball{
+    constructor(x, y, r) {
+        var options = {
+            'frictionAir':0.005,
+            'density':1
+        }
+        this.body = Bodies.circle(x, y, r/2 , options);
+        this.r= r;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        ellipse(0,0,this.r,this.r)
+        pop();
+      }
+}
